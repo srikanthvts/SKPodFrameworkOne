da57b057182832d02b5646a73cdd319b1284c584
Your NameSrikanth <srikanthp@yupptv.com>
2018-05-25 14:53:53 +0530

    first commit

---
+++ SKPodFrameworkOne/SKPodFrameworkOne/SKPodFrameworkOne.h
@@ -0,0 +1,19 @@
+//
+//  SKPodFrameworkOne.h
+//  SKPodFrameworkOne
+//
+//  Created by Srikanth on 5/25/18.
+//  Copyright © 2018 Srikanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKPodFrameworkOne.
+FOUNDATION_EXPORT double SKPodFrameworkOneVersionNumber;
+
+//! Project version string for SKPodFrameworkOne.
+FOUNDATION_EXPORT const unsigned char SKPodFrameworkOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKPodFrameworkOne/PublicHeader.h>
+
+
---
+++ SKPodFrameworkOne/SKPodFrameworkOne/Services.swift
@@ -0,0 +1,17 @@
+//
+//  Services.swift
+//  SKPodFrameworkOne
+//
+//  Created by Srikanth on 5/25/18.
+//  Copyright © 2018 Srikanth. All rights reserved.
+//
+
+import Foundation
+public class Services{
+    
+    private init(){}
+    
+    public static func sayHello() -> String{
+        return "Hello"
+    }
+}
